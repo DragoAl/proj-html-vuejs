a0df4c193e20185f46bf154aa761b953e250ae55
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-12-13 16:18:01 +0100

    Add: feat  header nav

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,65 @@
 <template>
   <div id="app">
+    <MyHeader
+    :menuElements = 'menuItems'
+    />
   </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    MyHeader
+  },
+
+  data() {
+    return {
+     menuItems: [
+      {
+        icon: "fab fa-pagelines",
+        title: 'TREATMENTS',
+        subtitle: 'Face & Body'
+        
+      },
+      
+      {
+        icon: "fas fa-male",
+        title: 'ABOUT',
+        subtitle: 'Our Team'
+        
+      },
+      
+      {
+        icon: "fas fa-heart",
+        title: 'JOURNAL',
+        subtitle: 'Tips & Tricks'
+        
+      },
+      
+      {
+        icon: "fas fa-bookmark",
+        title: 'BOOK NOW',
+        subtitle: 'Special Offers'
+        
+      },
+     ] 
+
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,32 @@
+<template>
+<nav>
+    <i class="fas fa-home"></i>
+    <div v-for= "element, i in menuElements " :key='i'>
+        <i :class="element.icon"></i>
+        <div>{{element.title}}</div>
+        <div>{{element.subtitle}}</div>
+
+    </div>
+</nav>
+    
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+    props : {
+        menuElements: Array
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+
+nav {
+    height: 100px;
+    display: flex;
+}
+
+
+</style>
