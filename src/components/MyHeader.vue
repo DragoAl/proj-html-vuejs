844af7cfed5d69d0a7c1ee8ffc8c5b8cfb5a81f4
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-12-13 16:43:12 +0100

    Add: nav logo and style nav

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,12 +1,19 @@
 <template>
 <nav>
     <i class="fas fa-home"></i>
-    <div v-for= "element, i in menuElements " :key='i'>
+    <div v-for= "element, i in menuElements.slice(0,2) " :key='i'>
         <i :class="element.icon"></i>
-        <div>{{element.title}}</div>
-        <div>{{element.subtitle}}</div>
-
+        <h5>{{element.title}}</h5>
+        <span>{{element.subtitle}}</span>
     </div>
+    <img src="../assets/img/avada-spa-logo-retina-new-400x46.png" alt="">
+    <div v-for= "element, i in menuElements.slice(2,4) " :key='i'>
+        <i :class="element.icon"></i>
+        <h5>{{element.title}}</h5>
+        <span>{{element.subtitle}}</span>
+    </div>
+    <i class="fas fa-plus"></i>
+
 </nav>
     
 </template>
@@ -26,6 +33,30 @@ export default {
 nav {
     height: 100px;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    div {
+        margin:0 25px;
+        cursor: pointer;
+    }
+    i {
+        color: #d3cac0;
+    }
+    img {
+        height: 25px;
+    }
+    h5 {
+        margin: 3px 0;
+        color: #6b6a69;
+    }
+    span{
+        font-size: 12px;
+        color: #6c6c6c;
+    }
+}
+.fa-home, .fa-plus {
+    margin: 0 40px;
+    color: #6b6a69;
 }
 
 
