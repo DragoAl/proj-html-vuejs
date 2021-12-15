ca39979d40925d806e7a56f0df483b119a7e35c1
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-12-15 11:26:18 +0100

    Add: Treatment card in treatSection

---
+++ src/components/TreatmentCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card" >
+        <img :src="require(`@/assets/img/${details.img}`)" :alt="details.treatName">
+        <h3>{{details.treatName}}</h3>
+        <div class="separator"></div>
+        <p>{{details.treatScope}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TreatmentCard',
+    props : {
+        details:Object
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.card{
+    width:25%;
+    text-align: left;
+    border: 0.1em solid #dadada;
+    cursor: pointer;
+
+    img {
+        width: 100%;
+    }
+
+    h3 {
+        font-family: 'Libre Baskerville', serif;
+        font-weight: 100;
+        padding: 23px;
+        color:#bcafa1 ;
+
+    }
+    p{
+        padding: 23px;
+    }
+    .separator{
+        width: 90%;
+        margin: 0 auto;
+        border: 0.1em dotted 	#dadada ;
+        
+    }
+}
+
+</style>
---
+++ src/components/TreatmentsSection.vue
@@ -0,0 +1,81 @@
+<template>
+    <section>
+        <TreatmentCard
+        v-for="treatment, i in treatmentsList " :key="i"
+        :details='treatment'
+        />
+    </section>
+</template>
+
+<script>
+import TreatmentCard from './TreatmentCard.vue'
+
+export default {
+    name: 'TreatmentsSection',
+    components: {
+        TreatmentCard
+    },
+    data () {
+        return {
+            treatmentsList: [
+                {
+                    img: 'Layer-1-new-400x440.jpg',
+                    treatName: 'Hydrotherapy',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'Layer-2-new-400x440.jpg',
+                    treatName: 'Massage',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'Layer-3-new-400x440.jpg',
+                    treatName: 'Aromatherapy',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'Layer-4-new-400x440.jpg',
+                    treatName: 'Heat Therapy',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'Layer-5-new-400x440.jpg',
+                    treatName: 'Skin Care',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'Layer-6-new-400x440.jpg',
+                    treatName: 'Spa Days',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'layer8-new-400x440.jpg',
+                    treatName: 'Beauty Treatments',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+                
+                {
+                    img: 'Layer-7-new-400x440.jpg',
+                    treatName: 'Residential',
+                    treatScope: 'Vestibulum nec velit ante. Praesent dignissim interdum est, in lacinia elit pretium nec. Aliquam erat voluptat. Fusce laoreet mi leo.'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+section {
+    display: flex;
+    flex-wrap: wrap;
+   
+}
+
+</style>
